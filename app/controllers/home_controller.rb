98fc1259f95b8dde9f2aad8314741f662dc1b829
Damien Banville <damienbanville@hotmail.fr>
2023-12-12 18:26:58 +0800

    Added faker and started to create seeds

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
 
 def index
+    if current_user
+        render 'profiles/index'
+    end
 end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+Faker::Config.locale = 'fr'
+User.delete_all
+# Profile.delete_all
+Skill.delete_all
+
+33.times do
+    User.create!(
+      email:     Faker::Internet.email,
+      password:   "password",
+    )
+end
+
+puts "#{User.all.count} users created"
+
+k = 1
+titles = ["Développeur fullstack", "Développeur Backend", "Développeur frontend", "Designer", "Consultant marketing", "Manager de projet", "Product manager"]
+experiences = ["0-2 ans", "3-7 ans", "8 ans et +"]
+categories = ["Développeur fullstack", "Développeur Backend", "Développeur frontend", "Designer", "Consultant marketing", "Manager de projet", "Product manager"]
+prices = [250, 300, 350, 400, 450, 500, 550, 600, 650]
+33.times do
+  Profile.create(
+    user_id: k
+    first_name: Faker::Name.first_name
+    last_name: Faker::Name.last_name
+    title: titles.sample
+    location: Faker::Address.city
+    price: prices.sample
+    experience: experiences.sample
+    is_remote: [true, false].sample
+    category: categories.sample
+  )
+  k += 1
+end
+
+puts "#{Profile.all.count} profiles created"
+
+skills = ["HTML", "CSS", "PHP", "Laravel", "Javascript", "RubyOnRails", "Git", "SQL", "Design"]
+
+skills.each do |s|
+    Skill.create(
+        skill: s
+    )
+end
+
+80.times do
+    u = User.find(rand(User.first.id..User.last.id)).id
+    s = Skill.find(rand(Skill.first.id..Skill.last.id)).id
+    if UserSkill.where('user_id', u).where('skill_id', s).empty?
+        UsersSkill.create(
+            user_id: u
+            skill_id: s
+        )
+    end
+end
